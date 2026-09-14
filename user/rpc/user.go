3ee10c1c68c2317bb0f3bb7db99b2b96f223bb3a
chenyukang <chenyukang90@outlook.com>
2026-09-14 18:36:43 +0800

    feat: 新增grpc服务端中间件功能, token有无鉴权 + 流式调用日志

--- user/rpc/user.go
+++ user/rpc/user.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 
@@ -10,10 +11,14 @@ import (
 	"user/rpc/user"
 
 	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/service"
 	"github.com/zeromicro/go-zero/zrpc"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 var configFile = flag.String("f", "rpc/etc/user.yaml", "the config file")
@@ -34,6 +39,27 @@ func main() {
 	})
 	defer s.Stop()
 
+	s.AddUnaryInterceptors(authInterceptor)
+	s.AddStreamInterceptors(loggingStreamInterceptor)
+
 	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
 	s.Start()
+}
+
+func authInterceptor(ctx context.Context, req any, info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler) (any, error) {
+	md, _ := metadata.FromIncomingContext(ctx)
+	if len(md["token"]) == 0 {
+		return nil, status.Error(codes.Unauthenticated, "缺少 token")
+	}
+	return handler(ctx, req)
+}
+func loggingStreamInterceptor(srv any, ss grpc.ServerStream,
+	info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	logx.Infof("流式调用开始: %s", info.FullMethod)
+	err := handler(srv, ss)
+	if err != nil {
+		logx.Errorf("流式调用错误: %s — %v", info.FullMethod, err)
+	}
+	return err
 }
