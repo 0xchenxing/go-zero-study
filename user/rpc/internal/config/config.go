f7c68a86795e904cdb8418b9f93f7363e92a0cc0
chenyukang <chenyukang90@outlook.com>
2026-09-15 17:39:33 +0800

    feat: 新增grpc数据库mysql和缓存redis功能

--- user/rpc/internal/config/config.go
+++ user/rpc/internal/config/config.go
@@ -1,7 +1,13 @@
 package config
 
-import "github.com/zeromicro/go-zero/zrpc"
+import (
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	"github.com/zeromicro/go-zero/zrpc"
+)
 
 type Config struct {
 	zrpc.RpcServerConf
+	DB         sqlx.SqlConf
+	CacheRedis cache.CacheConf
 }
--- user/rpc/internal/svc/servicecontext.go
+++ user/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,21 @@
 package svc
 
-import "user/rpc/internal/config"
+import (
+	"user/rpc/internal/config"
+	"user/rpc/model"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config    config.Config
+	UserModel model.UserModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	conn := sqlx.NewMysql(c.DB.DataSource)
 	return &ServiceContext{
-		Config: c,
+		Config:    c,
+		UserModel: model.NewUserModel(conn, c.CacheRedis),
 	}
 }
